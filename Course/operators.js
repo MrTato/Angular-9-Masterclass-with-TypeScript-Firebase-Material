1707aa1c125b5fa7fff9e203d5a1ce59403863d1
Bayardo <bjlslopez@gmail.com>
2020-04-19 10:11:40 -0600

    13. Operators - Destructure and Rest

--- Course/operators.js
+++ Course/operators.js
@@ -9,6 +9,17 @@ var __assign = (this && this.__assign) || function () {
     };
     return __assign.apply(this, arguments);
 };
+var __rest = (this && this.__rest) || function (s, e) {
+    var t = {};
+    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
+        t[p] = s[p];
+    if (s != null && typeof Object.getOwnPropertySymbols === "function")
+        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
+            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
+                t[p[i]] = s[p[i]];
+        }
+    return t;
+};
 var __spreadArrays = (this && this.__spreadArrays) || function () {
     for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
     for (var r = Array(s), k = 0, i = 0; i < il; i++)
@@ -58,3 +69,23 @@ console.log("Got the userDetails as: ", userDetails);
 var fName = user.firstName, lName = user.lastName;
 console.log(fName);
 console.log(lName);
+// Array Destructure
+var apple = fruits[0], greenApple = fruits[1], grapes = fruits[2];
+console.log(apple);
+console.log(greenApple);
+console.log(grapes);
+var justApple = edibles[0], restEdibles = edibles.slice(1);
+console.log(justApple);
+console.log(restEdibles);
+var firstName = userDetails.firstName, restUserDetails = __rest(userDetails, ["firstName"]);
+console.log(firstName);
+console.log(restUserDetails);
+function getEdibles(apple) {
+    var restEdibles = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        restEdibles[_i - 1] = arguments[_i];
+    }
+    console.log(apple);
+    console.log(restEdibles);
+}
+getEdibles.apply(void 0, edibles);
--- Course/operators.ts
+++ Course/operators.ts
@@ -60,3 +60,26 @@ console.log(`The user with name: ${user.firstName} has an address of: ${userDeta
  let { firstName: fName, lastName: lName} = user;
 console.log(fName);
 console.log(lName);
+
+// Array Destructure
+
+let [apple, greenApple, grapes] = fruits;
+
+console.log(apple);
+console.log(greenApple);
+console.log(grapes);
+
+let [ justApple, ... restEdibles] = edibles;
+console.log(justApple);
+console.log(restEdibles);
+
+let {firstName, ... restUserDetails} = userDetails;
+console.log(firstName);
+console.log(restUserDetails );
+
+function getEdibles(apple? , ... restEdibles) {
+    console.log(apple);
+    console.log(restEdibles);
+}
+
+getEdibles(... edibles);
