ba43a0e5fd354310902a910d644e741d67e4165e
Bayardo <bjlslopez@gmail.com>
2020-05-07 22:14:02 -0600

    23. declarations

--- my-shiny-new-angular-app/src/app/app.module.ts
+++ my-shiny-new-angular-app/src/app/app.module.ts
@@ -1,16 +1,25 @@
+import { CoreModule } from './core/core.module';
+import { HeaderComponent } from './core/header/header.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { TransformDirective } from './transform.directive';
+import { SafePipe } from './pipes/safe/safe.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    TransformDirective,
+    SafePipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ my-shiny-new-angular-app/src/app/transform.directive.spec.ts
@@ -0,0 +1,8 @@
+import { TransformDirective } from './transform.directive';
+
+describe('TransformDirective', () => {
+  it('should create an instance', () => {
+    const directive = new TransformDirective();
+    expect(directive).toBeTruthy();
+  });
+});
