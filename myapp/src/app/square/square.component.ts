6ddc92f834e72e62aff01e2f739a5cda62b78db5
Bayardo Lopez <b.lopez@mlsdatatools.com>
2020-05-16 12:47:47 -0600

    Angular for Beginners - Let's build a Tic-Tac-Toe PWA

---
+++ myapp/src/app/square/square.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-square',
+  template: `
+    <button nbButton *ngIf="!value">{{ value }}</button>
+    <button nbButton hero status="success" *ngIf="value == 'X'">{{ value }}</button>
+    <button nbButton hero status="info" *ngIf="value == 'O'">{{ value }}</button>
+  `,
+  styles: [
+  ]
+})
+export class SquareComponent {
+  @Input() value: 'X' | 'O';
+}
