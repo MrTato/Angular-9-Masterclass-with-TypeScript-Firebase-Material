f4c9c1528e8425af8f5740b81db378c30ae2eaba
Bayardo <bjlslopez@gmail.com>
2020-05-24 13:51:23 -0600

    42. View Queries - View Child, View Children, Content Child, Content Children

--- MYFIRSTANGULARSAPP/src/app/components/message-list/message-list.component.ts
+++ MYFIRSTANGULARSAPP/src/app/components/message-list/message-list.component.ts
@@ -1,26 +1,52 @@
-import { Component, OnInit, Input, Output } from '@angular/core';
+import { MessageComponent } from './../message/message.component';
+import { Component, OnInit, ViewChild, AfterViewInit, ViewChildren, QueryList, ContentChild, AfterContentInit, ContentChildren } from '@angular/core';
 
 @Component({
   selector: 'app-message-list',
   templateUrl: './message-list.component.html',
   styleUrls: ['./message-list.component.css']
 })
-export class MessageListComponent {
+export class MessageListComponent implements OnInit, AfterViewInit, AfterContentInit{
 
-  messages: Array<{ message: string }> = [];
-  message: string = '';
+  // This allows the parent component to have access to a child component
+  @ViewChild(MessageComponent) firstMessageComponent: MessageComponent;
+  @ViewChildren(MessageComponent) allMessageComponents: QueryList<MessageComponent>;
 
-  addAMessage() {
-    this.messages.push({ message: this.message });
-    this.message = '';
+  @ContentChild(MessageComponent) firstProjectedMessageComponent: MessageComponent;
+  @ContentChild(`tempMessage`) tempMessageComponent: MessageComponent;
+  @ContentChildren(MessageComponent) allProjectedMessageComponents: QueryList<MessageComponent>;
+
+  messages: string[] = [
+    "Message 1",
+    "Message 2",
+    "Message 3",
+    "Message 4"
+  ];
+
+  constructor() {
   }
 
-  onMessageDelete($event) {
-    this.messages.splice($event, 1);
+  ngAfterViewInit() {
+    this.firstMessageComponent.message = `This was changed from the message list component's ngAfterViewInit method`;
+    this.allMessageComponents.toArray().forEach(message => {
+      if(message.message !== `This was changed from the message list component's ngAfterViewInit method`) {
+        message.message = `This was changed from the message list component's ngAfterViewInit method using ViewChildren`;
+      }
+    });
   }
 
-  changeFirstMessage() {
-    this.messages[0].message = 'New and changed Message!';
+  ngAfterContentInit() {
+    this.firstProjectedMessageComponent.message = `The projected message was also changed`;
+    this.allProjectedMessageComponents.toArray().forEach(message => {
+      if(message.message !== `The projected message was also changed`) {
+        message.message = `The projected message was also changed using ContentChildren`;
+      }
+    });
+    this.tempMessageComponent.message = `The projected message was also changed using a template variable`;
+  }
+
+  ngOnInit() {
+
   }
 
 }
