c3d88ea7364c0818d73ce03f9a21e11a84ce0004
Bayardo <bjlslopez@gmail.com>
2020-06-28 03:56:25 -0600

    91. Creating a Reactive form in the TypeScript class & connecting it to the template

--- MYFIRSTANGULARSAPP/src/app/components/new-user-reactive/new-user-reactive.component.ts
+++ MYFIRSTANGULARSAPP/src/app/components/new-user-reactive/new-user-reactive.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-user-reactive',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewUserReactiveComponent implements OnInit {
 
+  userForm: FormGroup;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.userForm = new FormGroup({
+      name: new FormControl('John Doe'),
+      username: new FormControl('johndoe'),
+      email: new FormControl('john.doe@example.com'),
+      phone: new FormControl('9876543210'),
+      website: new FormControl('john.com'),
+      address: new FormGroup({
+        street: new FormControl('123 NE St'),
+        suite: new FormControl('Suite 1400'),
+        city: new FormControl('Believe'),
+        zipcode: new FormControl('98004'),
+        geo: new FormGroup({
+          lat: new FormControl('123.34434'),
+          lng: new FormControl('-12321.334'),
+        }),
+      }),
+      company: new FormGroup({
+        name: new FormControl('Beauty'),
+        catchPhrase: new FormControl('See Wold Differently'),
+        bs: new FormControl('A company that creates beautiful content for travel boards and hotel chains'),
+      }),
+    });
   }
 
   submit() {
