67a036fb35f6a91f7878609813a9911c4631054d
Bayardo <bjlslopez@gmail.com>
2020-08-21 17:26:07 -0600

    101. Adding Material Design Buttons

--- MyFirstMD/src/app/app-routing.module.ts
+++ MyFirstMD/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
+import { ButtonsComponent } from './components/buttons/buttons.component';
 import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
 const appRoutes: Routes = [
-  { path: 'home', component: HomeComponent }
+  { path: 'home', component: HomeComponent },
+  { path: 'buttons', component: ButtonsComponent },
 ];
 
 @NgModule({
--- MyFirstMD/src/app/app.module.ts
+++ MyFirstMD/src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
+import { ButtonsComponent } from './components/buttons/buttons.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    ButtonsComponent
   ],
   imports: [
     BrowserModule,
