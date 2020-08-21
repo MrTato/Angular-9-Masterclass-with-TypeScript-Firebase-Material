39a9011070049a3fe6f46e8c14a9fa661e25aceb
Bayardo <bjlslopez@gmail.com>
2020-08-21 13:42:35 -0600

    100. Adding a Navbar in your header

--- MyFirstMD/src/app/app-routing.module.ts
+++ MyFirstMD/src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
+import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- MyFirstMD/src/app/app.module.ts
+++ MyFirstMD/src/app/app.module.ts
@@ -5,10 +5,14 @@ import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
