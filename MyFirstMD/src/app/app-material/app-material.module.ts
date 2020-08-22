0f3004de2735aa162b323defd2361c38a7ed14dd
Bayardo <bjlslopez@gmail.com>
2020-08-22 13:04:33 -0600

    104. Introducing Angular Material into your Angular CLI Project on the top of MDB

---
+++ MyFirstMD/src/app/app-material/app-material.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    MatCheckboxModule
+  ],
+  exports: [
+    MatCheckboxModule
+  ]
+})
+export class AppMaterialModule { }
--- MyFirstMD/src/app/app.module.ts
+++ MyFirstMD/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { AppMaterialModule } from './app-material/app-material.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -10,6 +11,7 @@ import { HomeComponent } from './components/home/home.component';
 import { ButtonsComponent } from './components/buttons/buttons.component';
 import { CardsComponent } from './components/cards/cards.component';
 import { FormsComponent } from './components/forms/forms.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -23,7 +25,9 @@ import { FormsComponent } from './components/forms/forms.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MDBBootstrapModule.forRoot()
+    MDBBootstrapModule.forRoot(),
+    BrowserAnimationsModule,
+    AppMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
