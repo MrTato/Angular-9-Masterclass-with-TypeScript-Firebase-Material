a4fb674c785e88677d38d062a21f87986430cd30
Bayardo <bjlslopez@gmail.com>
2020-08-22 16:32:53 -0600

    105. Adding Radio Buttons, Slide Toggle, Slider, Progress Bar and Spinner

--- MyFirstMD/src/app/app-material/app-material.module.ts
+++ MyFirstMD/src/app/app-material/app-material.module.ts
@@ -1,15 +1,29 @@
 import { NgModule } from '@angular/core';
 import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 
 
 @NgModule({
-  declarations: [],
   imports: [
-    MatCheckboxModule
+    MatCheckboxModule,
+    MatRadioModule,
+    MatSlideToggleModule,
+    MatSliderModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
   ],
   exports: [
-    MatCheckboxModule
+    MatCheckboxModule,
+    MatRadioModule,
+    MatSlideToggleModule,
+    MatSliderModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
   ]
 })
 export class AppMaterialModule { }
